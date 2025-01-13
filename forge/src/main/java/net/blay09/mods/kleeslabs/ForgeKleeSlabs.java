749a836c07d545af70dfed2607bbd154f9c790df
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-13 20:46:30 +0100

    feat: Update for Forge 1.21.4

--- forge/src/main/java/net/blay09/mods/kleeslabs/ForgeKleeSlabs.java
+++ forge/src/main/java/net/blay09/mods/kleeslabs/ForgeKleeSlabs.java
@@ -1,19 +1,22 @@
 package net.blay09.mods.kleeslabs;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.blay09.mods.kleeslabs.client.KleeSlabsClient;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(KleeSlabs.MOD_ID)
 public class ForgeKleeSlabs {
 
-    public ForgeKleeSlabs() {
-        Balm.initialize(KleeSlabs.MOD_ID, EmptyLoadContext.INSTANCE, KleeSlabs::initialize);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(KleeSlabs.MOD_ID, EmptyLoadContext.INSTANCE, KleeSlabsClient::initialize));
+    public ForgeKleeSlabs(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
+        Balm.initialize(KleeSlabs.MOD_ID, loadContext, KleeSlabs::initialize);
+        if (FMLEnvironment.dist.isClient()) {
+            BalmClient.initialize(KleeSlabs.MOD_ID, loadContext, KleeSlabsClient::initialize);
+        }
     }
 
 }
