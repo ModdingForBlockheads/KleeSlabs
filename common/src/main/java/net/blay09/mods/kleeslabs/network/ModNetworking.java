92e81a60dfc1c9272f6317b84d87e87fe4e57cda
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-09 10:08:17 +0100

    fix: Enable server-only https://github.com/TwelveIterationMods/Balm/issues/122

--- common/src/main/java/net/blay09/mods/kleeslabs/network/ModNetworking.java
+++ common/src/main/java/net/blay09/mods/kleeslabs/network/ModNetworking.java
@@ -1,10 +1,12 @@
 package net.blay09.mods.kleeslabs.network;
 
 import net.blay09.mods.balm.api.network.BalmNetworking;
+import net.blay09.mods.kleeslabs.KleeSlabs;
 
 public class ModNetworking {
 
     public static void initialize(BalmNetworking networking) {
+        networking.allowServerOnly(KleeSlabs.MOD_ID);
         networking.registerClientboundPacket(KleeSlabsRegistryMessage.TYPE, KleeSlabsRegistryMessage.class, KleeSlabsRegistryMessage::encode, KleeSlabsRegistryMessage::decode, KleeSlabsRegistryMessage::handle);
     }
 
